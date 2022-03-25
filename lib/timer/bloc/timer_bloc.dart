42b7645058b0e9b4053fea2a4557b929e09388c5
Makai Pan <makai820117@gmail.com>
2022-03-26 00:33:51 +0800

    Complete TimerBloc

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
@@ -5,7 +7,40 @@ part 'timer_state.dart';
 part 'timer_event.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  int startedSeconds;
+  late int remainingSeconds;
+  late Timer _timer;
+  int startSeconds;
 
-  TimerBloc({required this.startedSeconds});
+  TimerBloc({required this.startSeconds})
+      : remainingSeconds = startSeconds,
+        super(TimerInitial()) {
+    on<TimerRunOneSecond>((event, emit) {
+      emit(TimerRunning());
+      _timer = Timer(const Duration(seconds: 1), () => add(TimerTick()));
+    });
+
+    on<TimerTick>((event, emit) {
+      remainingSeconds -= 1;
+      if (remainingSeconds > 0) {
+        add(TimerRunOneSecond());
+      } else {
+        emit(TimerComplete());
+      }
+    });
+
+    on<TimerPause>((event, emit) {
+      emit(TimerPausing());
+      _timer.cancel();
+    });
+
+    on<TimerResume>((event, emit) {
+      add(TimerRunOneSecond());
+    });
+
+    on<TimerReset>((event, emit) {
+      emit(TimerInitial());
+      _timer.cancel();
+      remainingSeconds = startSeconds;
+    });
+  }
 }
